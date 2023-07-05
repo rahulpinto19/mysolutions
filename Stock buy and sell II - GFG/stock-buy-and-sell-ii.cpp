31fbb89b11366142f7247b098d8451234f8e0f53
manuka rahul <96047526+rahulpinto19@users.noreply.github.com>
2023-07-05 23:05:01 +0530

    Added solution - LeetHub

--- Stock buy and sell II - GFG/stock-buy-and-sell-ii.cpp
+++ Stock buy and sell II - GFG/stock-buy-and-sell-ii.cpp
@@ -36,7 +36,7 @@ class Solution {
         int ans=0;
         for(int i=0;i<n-1;i++)
         if(prices[i]<prices[i+1])
-        ans+=prices[i+1]-prices[i];
+        ans+=(prices[i+1]-prices[i]);
         return ans;
     }
 };
